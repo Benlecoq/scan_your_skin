b6b8f582d20251ad0141155cccdff63f8fa84fd4
Liridone Zhugolli <liridonezhugolli@DESKTOP-V8L5PDI.localdomain>
2024-03-21 21:37:18 +0100

    ido4

--- front_end/pages/01_About_Melanoma.py
+++ front_end/pages/01_About_Melanoma.py
@@ -1,5 +1,25 @@
 import streamlit as st
 from PIL import Image
+import os
+
+# Function to get the absolute path of the image file
+def get_image_path(image_name):
+    # Get the directory of the current script
+    current_dir = os.path.dirname(__file__)
+    # Construct the path to the image
+    image_path = os.path.join(current_dir, 'images', image_name)
+    return image_path
+# Display content based on the active tab
+def display_side_content(active_tab):
+    if active_tab == "About Melanoma":
+        # Use the function to get the correct image path
+        image_path = get_image_path('about_1.jpg')
+        # Display the image using the resolved path
+        st.image(image_path, width=500)
+# Example usage
+#active_tab = "About Melanoma"  # Assuming this is set somewhere in your code
+#display_side_content(active_tab)
+
 
 # Sidebar
 st.sidebar.markdown('''
@@ -15,11 +35,10 @@ highlighted_text_title = (
 )
 st.markdown(highlighted_text_title, unsafe_allow_html=True)
 
-def display_side_content(side):
-    if side == "About":
-        st.image("images/about_1.jpg", width = 500)  # Display the image
-
-        # st.write("<span style=\"font-size:20px\">**Melanoma**</span>", unsafe_allow_html=True)
+def display_side_content(active_tab):
+    if active_tab == "About Melanoma":
+        image_path = get_image_path('about_1.jpg')
+        st.image(image_path, width=500)
 
         st.markdown("""
         - <span style="font-size:larger">most invasive skin cancer with the highest risk of death</span>
