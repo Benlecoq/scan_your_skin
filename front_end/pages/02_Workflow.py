29ce76ddf08c9157cfa3f4c66541a4ab3c4a4fb9
Jihyeong Lee <lee.jihyeong.426@gmail.com>
2024-03-22 20:36:49 +0100

    front_end_revised_14

--- front_end/pages/02_Workflow.py
+++ front_end/pages/02_Workflow.py
@@ -18,59 +18,60 @@ def get_image_path(image_name):
     return image_path
 
 # Function to display content for the first subpage
-def subpage1():
-    def display_side_content(side):
-        if side == "Data":
-            st.markdown("<span style=\"font-size:20px; color:gray;\">**Dataset**</span>", unsafe_allow_html=True)            
-            image_path = get_image_path('workflow_data.png')
-            st.image(image_path, width=1000)
-            st.markdown("- <span style=\"font-size:18px; color:gray;\">Kaggle dataset from the original **HAM10000** (Human Against Machine with 10,000 Training images) dataset.</span>", unsafe_allow_html=True)
+def display_side_content(side):
+    if side == "Data":
+        st.markdown("<span style=\"font-size:20px; color:gray;\">**Dataset**</span>", unsafe_allow_html=True)            
+        image_path = get_image_path('workflow_data.png')
+        st.image(image_path, width=1000)
+        st.markdown("- <span style=\"font-size:18px; color:gray;\">Kaggle dataset from the original **HAM10000** (Human Against Machine with 10,000 Training images) dataset.</span>", unsafe_allow_html=True)
             
-        elif side == "Preprocessing":
-            st.markdown("<span style=\"font-size:20px; color: gray;\">**Dataset Clustering**</span>", unsafe_allow_html=True)
-            st.markdown("- <span style=\"font-size:18px; color: gray;\">Data augmentation for balancing the data</span>\n"
-                        "- <span style=\"font-size:20px; color: gray;\">Reshaping of images to **224x224 pixels** for **CNN processing**, normalized</span>", unsafe_allow_html=True)
-            image_path = get_image_path('data_new.png')
-            st.image(image_path, width=1000)
+    elif side == "Preprocessing":
+        st.markdown("<span style=\"font-size:20px; color: gray;\">**Dataset Clustering**</span>", unsafe_allow_html=True)
+        st.markdown("- <span style=\"font-size:18px; color: gray;\">Data augmentation for balancing the data</span>\n"
+                    "- <span style=\"font-size:20px; color: gray;\">Reshaping of images to **224x224 pixels** for **CNN processing**, normalized</span>", unsafe_allow_html=True)
+        image_path = get_image_path('data_new.png')
+        st.image(image_path, width=1000)
 
-        elif side == "Process":
-            st.markdown("<span style=\"font-size:20px; color: gray;\">**From data to prediction**</span>", unsafe_allow_html=True)
-            image_path = get_image_path('process_new.png')
-            st.image(image_path, width=1200)
+    elif side == "Process":
+        st.markdown("<span style=\"font-size:20px; color: gray;\">**From data to prediction**</span>", unsafe_allow_html=True)
+        image_path = get_image_path('process_new.png')
+        st.image(image_path, width=1200)
 
-        elif side == "Kernels":
-            st.markdown("<span style=\"font-size:20px; color: gray;\">**What happens in the black box**</span>", unsafe_allow_html=True)
-            image_path = get_image_path('feature_extraction.png')
-            st.image(image_path, width=1000)
+    elif side == "Kernels":
+        st.markdown("<span style=\"font-size:20px; color: gray;\">**What happens in the black box**</span>", unsafe_allow_html=True)
+        image_path = get_image_path('feature_extraction.png')
+        st.image(image_path, width=1000)
 
-        elif side == "Prediction Model":
-            st.markdown("""
-                - <span style="font-size:25px; color: gray;">Learning Curves and Performance Metrics</span>
-                """, unsafe_allow_html=True)
-            image_path = get_image_path('learning_curves.png')
-            st.image(image_path, width=1000)
+    elif side == "Prediction Model":
+        st.markdown("""
+            - <span style="font-size:25px; color: gray;">Learning Curves and Performance Metrics</span>
+            """, unsafe_allow_html=True)
+        image_path = get_image_path('learning_curves.png')
+        st.image(image_path, width=1000)
     
     
-    def horizontal_tabs(default_tabs=[], default_active_tab=0):
-        selected_tab = st.session_state.get('selected_tab', default_tabs[default_active_tab])
+# Function to handle horizontal tabs    
+def horizontal_tabs(default_tabs=[], default_active_tab=0):
+    selected_tab = st.session_state.get('selected_tab', default_tabs[default_active_tab])
 
-        if not default_tabs:
-            return None
+    if not default_tabs:
+        return None
 
-        cols = st.columns(len(default_tabs))
-        for index, tab_name in enumerate(default_tabs):
-            with cols[index]:
-                if st.button(tab_name, key=tab_name):
-                    selected_tab = tab_name
+    cols = st.columns(len(default_tabs))
+    for index, tab_name in enumerate(default_tabs):
+        with cols[index]:
+            if st.button(tab_name, key=tab_name):
+                selected_tab = tab_name
 
-        st.session_state.selected_tab = selected_tab
-        return selected_tab
+    st.session_state.selected_tab = selected_tab
+    return selected_tab
 
-    tabs_list = ["Data", "Preprocessing", "Process", "Kernels", "Prediction Model"]
-    active_tab = horizontal_tabs(tabs_list)
+#Define tabs and get the currently active tab
+tabs_list = ["Data", "Preprocessing", "Process", "Kernels", "Prediction Model"]
+active_tab = horizontal_tabs(tabs_list)
 
-    if active_tab:
-        display_side_content(active_tab)
+if active_tab:
+    display_side_content(active_tab)
 
 
 
