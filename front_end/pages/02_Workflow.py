d0ff59cd72994fb24af4c2b99cc8ab41fe6702f1
Jihyeong Lee <lee.jihyeong.426@gmail.com>
2024-03-22 20:28:57 +0100

    front_end_revised_13

--- front_end/pages/02_Workflow.py
+++ front_end/pages/02_Workflow.py
@@ -27,6 +27,7 @@ def subpage1():
             st.markdown("- <span style=\"font-size:18px; color:gray;\">Kaggle dataset from the original **HAM10000** (Human Against Machine with 10,000 Training images) dataset.</span>", unsafe_allow_html=True)
             
         elif side == "Preprocessing":
+            st.markdown("<span style=\"font-size:20px; color: gray;\">**Dataset Clustering**</span>", unsafe_allow_html=True)
             st.markdown("- <span style=\"font-size:18px; color: gray;\">Data augmentation for balancing the data</span>\n"
                         "- <span style=\"font-size:20px; color: gray;\">Reshaping of images to **224x224 pixels** for **CNN processing**, normalized</span>", unsafe_allow_html=True)
             image_path = get_image_path('data_new.png')
@@ -35,13 +36,21 @@ def subpage1():
         elif side == "Process":
             st.markdown("<span style=\"font-size:20px; color: gray;\">**From data to prediction**</span>", unsafe_allow_html=True)
             image_path = get_image_path('process_new.png')
-            st.image(image_path, width=1000)
+            st.image(image_path, width=1200)
 
         elif side == "Kernels":
             st.markdown("<span style=\"font-size:20px; color: gray;\">**What happens in the black box**</span>", unsafe_allow_html=True)
             image_path = get_image_path('feature_extraction.png')
             st.image(image_path, width=1000)
 
+        elif side == "Prediction Model":
+            st.markdown("""
+                - <span style="font-size:25px; color: gray;">Learning Curves and Performance Metrics</span>
+                """, unsafe_allow_html=True)
+            image_path = get_image_path('learning_curves.png')
+            st.image(image_path, width=1000)
+    
+    
     def horizontal_tabs(default_tabs=[], default_active_tab=0):
         selected_tab = st.session_state.get('selected_tab', default_tabs[default_active_tab])
 
@@ -57,29 +66,11 @@ def subpage1():
         st.session_state.selected_tab = selected_tab
         return selected_tab
 
-    tabs_list = ["Data", "Preprocessing", "Process", "Kernels"]
+    tabs_list = ["Data", "Preprocessing", "Process", "Kernels", "Prediction Model"]
     active_tab = horizontal_tabs(tabs_list)
 
     if active_tab:
         display_side_content(active_tab)
 
-# Function to display content for the second subpage
-def subpage2():
-    st.markdown("""
-        - <span style="font-size:25px; color: gray;">Learning Curves and Performance Metrics</span>
-        """, unsafe_allow_html=True)
-    image_path1 = get_image_path('learning_curves.png')
-    st.image(image_path1, width=1000)
 
 
-
-
-
-# Create sidebar navigation menu
-subpage = st.sidebar.radio("Workflow", ["Data", "Prediction Model"])
-
-# Display content based on selected subpage
-if subpage == "Data":
-    subpage1()
-elif subpage == "Prediction Model":
-    subpage2()
